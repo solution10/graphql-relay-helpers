d6f68e0d7c0ec93891dea1467133ffa4b4b81fdc
Alex Gisby <alex@solution10.com>
2025-09-30 20:10:58 +1000

    chore: rename everything to be underscores

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Relay GraphQL spec helpers
 
+[![⚒️ Build and test](https://github.com/solution10/graphql-relay-helpers/actions/workflows/branch-test.yml/badge.svg)](https://github.com/solution10/graphql-relay-helpers/actions/workflows/branch-test.yml)
+
 WIP - real documentation coming soon, this is an experiment for now.
--- graphql_relay_helpers/src/lib.rs
+++ graphql_relay_helpers/src/lib.rs
@@ -1,9 +1,17 @@
+//! Library to help with working with the Relay specification. Contains derive macros for generating
+//! connection and edge structs, as well as structs for the Pagination information and handling
+//! of Relay cursors and Identifiers.
+//!
+//! # Macros
+//!
+//! ## RelayConnection
+//!
 //! The object that this is applied to must also `#[derive(juniper::GraphQLObject)]` otherwise you'll
 //! get a compilation error.
 //!
 //! Given the following struct:
 //!
-//! ```no_compile
+//! ```no_compile,rust
 //! use juniper::GraphQLObject;
 //! # use graphql_relay_helpers_codegen::RelayConnection;
 //!
@@ -16,7 +24,7 @@
 //!
 //! The `RelayConnection` macro will generate two additional structs:
 //!
-//! ```no_compile
+//! ```no_compile,rust
 //! use juniper::GraphQLObject;
 //! # use graphql_relay_helpers_codegen::RelayConnection;
 //!
